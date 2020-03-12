7b27269127f0801e8e18cfddd57fdbff047f9b25
LoebKalin <loeb.kalin79@gmail.com>
2020-03-12 16:18:39 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,27 +3,16 @@ import { Switch, Route, Redirect, BrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
 // import "./App.css";
 import Ecotourism from "./components/page/ecotourism";
-import Airwater from "./components/page/airwater";
-import NaturalFarming from "./components/page/natural-farming";
-import Reforestation from "./components/page/reforestration";
-import SeedBomb from "./components/page/seed-bomb";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       {/* <Ecotourism /> */}
-//       <Airwater />
-//       {/* <NaturalFarming /> */}
-//       {/* <Reforestation /> */}
-//       {/* <SeedBomb /> */}
-//     </div>
+// import Airwater from "./components/page/airwater";
+// import NaturalFarming from "./components/page/natural-farming";
+// import Reforestation from "./components/page/reforestration";
+// import SeedBomb from "./components/page/seed-bomb";
 
 function App() {
   return (
     <React.Fragment>
       <BrowserRouter>
         <Switch>
-          {/* <SinglePage /> */}
           <Ecotourism />
           <Route exact path="/" component={Home} />
           {/* <Route exact path="/reforestation" component={Reforestation}/>
--- src/components/page/seed-bomb.js
+++ src/components/page/seed-bomb.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Button } from "react-bootstrap";
 import "./singlePage.css";
 import Mardown from "react-markdown";
-import Footer from "./Footer";
+import Footer from "./Detail-Footer";
 
 const seedbomb = require("../data/seedBomb.md");
 
